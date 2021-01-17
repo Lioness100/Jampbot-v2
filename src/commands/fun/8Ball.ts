762d1b16342b2b376f25feb2d3081ab1de9af6f3
Lioness100 <jchickmm2@gmail.com>
2021-01-17 17:24:24 -0500

    feat(command): :sparkles: 8ball command

---
+++ src/commands/fun/8Ball.ts
@@ -0,0 +1,35 @@
+import type { CommandOptions } from 'discord-akairo';
+import type { Message } from 'discord.js';
+import { replies, emotes } from '../../lib/utils/Constants';
+import ApplyOptions from '../../lib/utils/ApplyOptions';
+import Command from '../../lib/structures/Command';
+
+interface Args {
+  question: string;
+}
+
+@ApplyOptions<CommandOptions>('8ball', {
+  aliases: ['8ball'],
+  description: 'Ask a question to the 8Ball',
+  usage: '<question>',
+  examples: ['should I do my homework'],
+  args: [
+    {
+      id: 'question',
+      match: 'rest',
+    },
+  ],
+})
+export default class EightBall extends Command {
+  public run(message: Message, { question }: Args): void {
+    void message.util!.send(
+      message
+        .embed(`The 8Ball Has Spoken ${emotes.hype}`)
+        .setColor('RANDOM')
+        .addFields([
+          { name: 'Question:', value: question },
+          { name: 'Answer:', value: this.client.util.sample(replies) },
+        ])
+    );
+  }
+}
