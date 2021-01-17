5aa3cf604fb757f5b48c0997b9c07e59c9b10652
Lioness100 <jchickmm2@gmail.com>
2021-01-17 08:38:22 -0500

    feat(command): :sparkles: new reload command

---
+++ src/commands/utility/Reload.ts
@@ -0,0 +1,45 @@
+import { CommandOptions, Argument, Listener, Inhibitor } from 'discord-akairo';
+import type { Message } from 'discord.js';
+import ApplyOptions from '../../lib/utils/ApplyOptions';
+import Command from '../../lib/structures/Command';
+
+interface Args {
+  module: Command | Listener | Inhibitor;
+}
+
+@ApplyOptions<CommandOptions>('reload', {
+  aliases: ['reload', 'refresh'],
+  description: 'Reload a module',
+  ownerOnly: true,
+  args: [
+    {
+      id: 'module',
+      match: 'text',
+      type: Argument.union('commandAlias', 'listener', 'inhibitor'),
+    },
+  ],
+})
+export default class Reload extends Command {
+  public run(message: Message, { module }: Args): void {
+    if (!module)
+      return void message.error("You didn't provide a valid module!");
+
+    try {
+      module.reload();
+      void message.util!.send(
+        message.embed(
+          `The "${
+            module.id
+          }" ${module.handler.classToHandle.name.toLowerCase()} was reloaded!`
+        )
+      );
+    } catch (e) {
+      console.log(
+        `There was an error when reloading the "${
+          module.id
+        }" ${module.handler.classToHandle.name.toLowerCase()}`
+      );
+      void message.error('Something went wrong', `\`\`\`${e}\`\`\``);
+    }
+  }
+}
