023228a6b2b4aa07a8d0211b8fe70f752930416b
Lioness100 <jchickmm2@gmail.com>
2021-01-26 20:09:07 -0500

    feat(command): :sparkles: ping leaderboard lol

--- src/commands/misc/Ping.ts
+++ src/commands/misc/Ping.ts
@@ -1,17 +1,46 @@
 import type { CommandOptions } from 'discord-akairo';
-import type { Message } from 'discord.js';
+import { Message, Collection } from 'discord.js';
 import ApplyOptions from '../../lib/utils/ApplyOptions';
 import Command from '../../lib/structures/Command';
+
+interface Args {
+  leaderboard: boolean;
+}
 
 @ApplyOptions<CommandOptions>('ping', {
   aliases: ['ping'],
   description: "View the client's ping",
+  args: [
+    {
+      id: 'leaderboard',
+      match: 'flag',
+      flag: ['--leaderboard', '--lb'],
+      description: 'View the ping leaderboard for this instance',
+    },
+  ],
 })
 export default class Ping extends Command {
-  public run(message: Message): void {
-    message.embed(
-      `:ping_pong:  Pong! My ping is ${this.client.ws.ping}ms!`,
-      true
-    );
+  private leaderboard = new Collection<string, number>();
+
+  public run(message: Message, { leaderboard }: Args): void {
+    if (leaderboard)
+      message.embed(`Ping Leaderboard`, (embed) =>
+        embed
+          .addFields(
+            this.leaderboard
+              .sort()
+              .map((value, name) => ({ name, value: `${value}ms` }))
+              .slice(0, 5)
+          )
+          .setFooter('Leaderboards are reset every restart')
+      );
+    else {
+      const ping = this.client.ws.ping;
+      message.embed(`:ping_pong:  Pong! My ping is ${ping}ms!`, true);
+
+      const previous = this.leaderboard.get(message.author.tag);
+      if (!previous || previous > ping)
+        this.leaderboard.set(message.author.tag, ping);
+    }
   }
 }
