cc84797d20b1b85112bcf5d3180c4b48c15c15c7
Lioness100 <jchickmm2@gmail.com>
2021-01-30 08:45:12 -0500

    fix(listener): :bug: i give up

--- src/listeners/client/GuildMemberAdd.ts
+++ src/listeners/client/GuildMemberAdd.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { GuildMember, TextChannel } from 'discord.js';
 import { Listener, ListenerOptions } from 'discord-akairo';
 import { createCanvas, loadImage, registerFont } from 'canvas';
@@ -11,48 +10,41 @@ import ApplyOptions from '../../lib/utils/ApplyOptions';
 })
 export default class GuildMemberAddListener extends Listener {
   public async exec(member: GuildMember): Promise<void> {
-    try {
-      registerFont('./assets/fonts/mario.ttf', {
-        family: 'mario',
-      });
-      const canvas = createCanvas(700, 250);
-      const ctx = canvas.getContext('2d');
+    registerFont('./assets/fonts/mario.ttf', {
+      family: 'mario',
+    });
+    const canvas = createCanvas(700, 250);
+    const ctx = canvas.getContext('2d');
 
-      const background = await loadImage(
-        fs.readFileSync('./assets/images/welcome.png')
-      );
-      ctx.drawImage(background, 0, 0, 700, 250);
+    const background = await loadImage(
+      'https://cdn.discordapp.com/attachments/795067938482814996/804501115268890694/file.jpg'
+    );
+    ctx.drawImage(background, 0, 0, 700, 250);
 
-      let font = 95;
-      let num = 1.4;
-      do {
-        num += 0.07;
-        ctx.font = `${(font -= 10)}px mario, malgun gothic, arial`;
-      } while (ctx.measureText(member.user.username.toUpperCase()).width > 450);
+    let font = 95;
+    let num = 1.4;
+    do {
+      num += 0.07;
+      ctx.font = `${(font -= 10)}px mario, malgun gothic, arial`;
+    } while (ctx.measureText(member.user.username.toUpperCase()).width > 450);
 
-      ctx.fillText(member.user.username.toUpperCase(), 250, 250 / num);
+    ctx.fillText(member.user.username.toUpperCase(), 250, 250 / num);
 
-      ctx.beginPath();
-      ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.clip();
+    ctx.beginPath();
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
 
-      const avatar = await loadImage(
-        member.user.displayAvatarURL({ format: 'png', size: 4096 })
-      );
-      ctx.drawImage(avatar, 25, 25, 200, 200);
+    const avatar = await loadImage(
+      member.user.displayAvatarURL({ format: 'png', size: 4096 })
+    );
+    ctx.drawImage(avatar, 25, 25, 200, 200);
 
-      void (member.guild.channels.cache.get(
-        channels.welcome
-      ) as TextChannel).send(
-        `Hey ${member}, welcome to **Team Jamp!** To gain access to the rest of the Discord, please read <#${channels.rules}> and agree to the message near the bottom!\n\Have a great time, and remember to contact a mod with any questions ${emotes.pog}`,
-        { files: [canvas.toBuffer()] }
-      );
-    } catch (e) {
-      this.client.logger.error(
-        `Paths are still a problem. Exists: `,
-        fs.existsSync('./assets/images/welcome.png')
-      );
-    }
+    void (member.guild.channels.cache.get(
+      channels.welcome
+    ) as TextChannel).send(
+      `Hey ${member}, welcome to **Team Jamp!** To gain access to the rest of the Discord, please read <#${channels.rules}> and agree to the message near the bottom!\n\Have a great time, and remember to contact a mod with any questions ${emotes.pog}`,
+      { files: [canvas.toBuffer()] }
+    );
   }
 }
